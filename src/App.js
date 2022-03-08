b340b8a44ce5bbd9672c603c220469bb5a41e1fe
Bijay Bhandari <bijay.bhandari007@gmail.com>
2022-03-07 19:49:18 -0500

    Using jsx for hello world

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './component/Hello.tsx';
+import Welcome from './component/Welcome.tsx';
 
 
 function App() {
   return (
     <div className="App">
      <Greet />
+     <Welcome/>
     </div>
   );
 }
---
+++ src/component/Welcome.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+//jsx use
+const Welcome = () =>{
+return(
+    <h1>Hi I am from jsx</h1>
+)
+
+}
+
+export default Welcome;
